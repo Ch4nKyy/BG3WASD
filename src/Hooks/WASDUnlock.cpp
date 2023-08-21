43b7ae656bcae713059bad8065da31858ef4b640
Ch4nKyy <ch4nkyy@gmail.com>
2023-08-21 09:57:39 +0200

    Change FATAL to WARN
    FATAL immediately crashes, so the degradation stuff was pointless before.

--- src/Hooks/WASDUnlock.cpp
+++ src/Hooks/WASDUnlock.cpp
@@ -20,7 +20,7 @@ bool WASDUnlock::Enable()
 
     if (not(addr1 && addr2))
     {
-        FATAL("WASD unlock not found! Mod is disabled!");
+        WARN("WASD unlock not found! Mod is disabled!");
         return false;
     }
 
