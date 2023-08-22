de17c6380b4d928177d9f9ec8b39e19bc61dce89
Ch4nKyy <ch4nkyy@gmail.com>
2023-08-22 17:27:01 +0200

    Update publish script
    I am paranoid, so I rebuild completely on publish.
    Also, adapt folder structure for Vortex.

--- publish.ps1
+++ publish.ps1
@@ -1,10 +1,12 @@
-if (!(Test-Path $PSScriptRoot\build\Release\*)) {
-    exit
-}
+Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
+& cmake -B $PSScriptRoot/build -S $PSScriptRoot --preset=REL -DPLUGIN_MODE:BOOL=TRUE
+& cmake --build $PSScriptRoot/build --config Release
 
-Remove-Item $PSScriptRoot\NativeMods -Force -Recurse -ErrorAction:SilentlyContinue | Out-Null
-Remove-Item $PSScriptRoot\*.zip -Force -Recurse -ErrorAction:SilentlyContinue | Out-Null
-New-Item -ItemType Directory -Path $PSScriptRoot\NativeMods | Out-Null
-Copy-Item $PSScriptRoot\build\Release\* $PSScriptRoot\NativeMods\
-Compress-Archive -Path $PSScriptRoot\NativeMods\ -DestinationPath $PSScriptRoot\BG3WASD.zip -Force
-Remove-Item $PSScriptRoot\NativeMods -Force -Recurse  -ErrorAction:SilentlyContinue | Out-Null
+Remove-Item $PSScriptRoot\package\bin -Force -Recurse -ErrorAction:SilentlyContinue | Out-Null
+Remove-Item $PSScriptRoot\package\*.zip -Force -Recurse -ErrorAction:SilentlyContinue | Out-Null
+New-Item -ItemType Directory -Path $PSScriptRoot\package\bin | Out-Null
+New-Item -ItemType Directory -Path $PSScriptRoot\package\bin\NativeMods | Out-Null
+Copy-Item $PSScriptRoot\build\Release\* $PSScriptRoot\package\bin\NativeMods\
+Copy-Item $PSScriptRoot\package\*.toml $PSScriptRoot\package\bin\NativeMods\
+Compress-Archive -Path $PSScriptRoot\package\bin -DestinationPath $PSScriptRoot\package\BG3WASD.zip -Force
+Remove-Item $PSScriptRoot\package\bin -Force -Recurse  -ErrorAction:SilentlyContinue | Out-Null
